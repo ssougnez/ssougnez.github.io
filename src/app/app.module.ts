8478746dd1ff307111ab18e9e353ca2690030798
ssougnez <s.sougnez@areaprog.com>
2021-08-01 01:25:22 +0200

    First light version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BadgeModule, ButtonModule, IconModule } from '@ssougnez/ngx-ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    IconModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
